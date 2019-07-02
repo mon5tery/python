a8dc399e571627309621f20d80815d123470b49a
ahalrasheed <ahmed.alrasheed88@gmail.com>
2019-07-02 20:41:26 +0300

    Finishing the functions assignment

--- functions_task.py
+++ functions_task.py
@@ -1,33 +1,46 @@
 from datetime import date
 
-present = date.today()
 
 #birthdate = (year, month, day)
 def check_birthdate(year, month, day):
-	if present > date(year, month, day):
+	present = date.today()
+	birthdate = date(year, month, day)
+	if present > birthdate:
 		return(True)
 	else:
 		return(False)
 
 def calculate_age(year, month, day):
-	dob = present - date(year, month, day)
-
-	year = present.year - year
-	month = present.month - month
-	day = present.day - day
+	present = date.today()
+	birthdate = date(year, month, day)
+	#returns timedelta which can be used in age.days.
+	age = present - birthdate
+	age_days = age.days
 	
-	print ("You\'re %d years, %d months, and %d days" % (year, month, day))
+	year = int(age_days / 365)
+	age_days = int(age_days % 365)
+	#age_days %= 365
+	month = int(age_days / 30)
+	age_days = int(age_days % 30)
+
+	#year = present.year - year
+	#month = present.month - month
+	#day = present.day - day
+	
+	#int can be put in the last print and doesn't have to be as the top
+	# I.E: print("You are %d years, %d months, and %d days" % (int(year),int(month), age_days)
+	print ("You\'re %d years, %d months, and %d days" % (year, month, age_days))
 	
 
 
 
 	#print (present - birthdate)
-
+#make sure that the primary variables (year, month, day) are integers from the root.
 year = int(input("Enter year of birth: "))
 month = int(input("Enter month of birth: "))
 day = int(input("Enter day of birth: "))
 
-if check_birthdate(year, month, day) == True:
+if check_birthdate(year, month, day):
 	calculate_age(year, month, day)
 else:
 	print ("The birthdate is invalid")
